2a989b08e00b7d6fdf458082aa3ab5b4c9dc95c4
Kasper Hartwich <kasperhartwich@gmail.com>
2026-04-03 21:33:10 +0200

    fix: remove mapbox CSP worker import and guard Echo init behind Reverb app key check

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,9 +2,7 @@ import './bootstrap';
 
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
-import mapboxWorker from 'mapbox-gl/dist/mapbox-gl-csp-worker?url';
 import QRCode from 'qrcode';
 
-mapboxgl.workerUrl = mapboxWorker;
 window.mapboxgl = mapboxgl;
 window.QRCode = QRCode;
